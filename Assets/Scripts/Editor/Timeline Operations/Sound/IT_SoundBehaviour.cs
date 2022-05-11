5aa829d50ddb5c7cccde56b016854599fdaed5f9
Ertan Turan <ertan@retinize.com>
2022-05-11 11:56:02 +0300

    test behaviour added

---
+++ Assets/Scripts/Editor/Timeline Operations/Sound/IT_SoundBehaviour.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[Serializable]
+public class IT_SoundBehaviour : PlayableBehaviour
+{
+    public Color  Color;
+    public float  SomeTemporaryValue1;
+    public int    SomeTemporaryValue2;
+    public string SomeTemporaryValue3;
+}
