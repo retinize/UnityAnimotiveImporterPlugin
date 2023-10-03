204e59946faa9d582cf06566b0f38a88edc0503b
Ertan <ertan@retinize.com>
2023-10-03 13:54:24 +0100

    commented clear accumulation button

--- Assets/AnimotiveImporterScripts/Editor/Core Editor/IT_AnimotiveImporterEditorWindow.cs
+++ Assets/AnimotiveImporterScripts/Editor/Core Editor/IT_AnimotiveImporterEditorWindow.cs
@@ -140,7 +140,7 @@ namespace Retinize.Editor.AnimotiveImporter
 
             #endregion
 
-            if (GUILayout.Button("Clear Accumulation")) ClearAccumulatedFiles();
+            // if (GUILayout.Button("Clear Accumulation")) ClearAccumulatedFiles();
         }
 
         /// <summary>
